b613f4e47649372183c1d87f2090a1dd845ccbe8
Tyler Harriott <tharriott02@gmail.com>
2019-02-20 19:40:02 -0500

    Updating to have only the greedy search

--- src/UniformCostSearch/ChangeClass.java
+++ src/UniformCostSearch/ChangeClass.java
@@ -16,24 +16,13 @@ public class ChangeClass {
         System.out.println("Enter the amount you want change for: ");
         int amount = input.nextInt();
 
-        System.out.println("Press \n"
-                + "1: Greedy Algorithm \n"
-                + "2: Dynamic Programming");
-
-        int choice = input.nextInt();
-
-        switch(choice){
-            case 1:
-                greedyAlgorithim(amount,coins);
-                break;
-            case 2 :
-                dynamicPogrammingAlgorithim(amount,coins);
-                break;
-        }
-
+        greedyAlgorithim(amount,coins);
+        
         printResult();
     }
-
+    
+//////////////////////////////////////////////////////////////////////////////
+    
     public static int greedyAlgorithim(int amount,int[] coins){
 
         for(int i=coins.length-1; i >= 0 && amount > 0;i--){
@@ -45,36 +34,14 @@ public class ChangeClass {
         }
         return resultSet.size();
     }
-
+    
+/////////////////////////////////////////////////////////////////////////////////
+    
     public static void printResult(){
         System.out.println("Number of coins = " + resultSet.size());
         for ( int i =0;i<resultSet.size();i++){
             System.out.println(resultSet.get(i));
         }
-    }
-
-    public static int dynamicPogrammingAlgorithim(int amount,int[] coins){
-
-        int[] tempArray = new int[amount+1];
-        int[] prev = new int[amount+1];
-        tempArray[0]=0;
-        for(int j=1;j<=amount;j++){
-            tempArray[j]=Integer.MAX_VALUE;
-            for(int i=0;i<coins.length;i++){
-                if(coins[i]<=j && (1+tempArray[j-coins[i]] < tempArray[j]) ){
-                    tempArray[j]=1+tempArray[j-coins[i]];
-                    prev[j]=coins[i];
-                }
-            }
-        }
-        int result = tempArray[amount];
-        for(int i=amount;i>=1;){
-            resultSet.add(prev[i]);
-            int j=i;
-            i=amount-prev[i];
-            amount = amount - prev[j];
-        }
-        return resultSet.size();
     }
 
 
